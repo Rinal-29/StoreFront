cde07e5afe880b1bcb58298c83309f0b81e93136
Rinal-29 <54401937+Rinal-29@users.noreply.github.com>
2021-12-10 14:33:26 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script/navbar-scroll.js
@@ -0,0 +1,7 @@
+// add  bg when scroll navbar
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $('.navbar-fixed-top');
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
